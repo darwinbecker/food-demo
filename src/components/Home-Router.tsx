963f87e204d6e323169e35fde52b7fbea6d3961d
Darwin Becker <darwinbecker@web.de>
2022-03-07 23:33:55 +0100

    changes for deployment

--- src/components/Home-Router.tsx
+++ src/components/Home-Router.tsx
@@ -4,7 +4,7 @@ import {
     Routes,
     Route,
     Link,
-    useParams
+    Navigate
 } from "react-router-dom";
 import FoodPage from './Food/FoodPage';
 import DiaryPage from './Diary/DiaryPage';
@@ -16,19 +16,22 @@ export const Home: React.FC = () => {
             <section>
                 <p>This is a small demo of my food project, that I sometimes work on.</p>
                 <p>
-                    In this project you can display and calculate the nutritional values of a specific food item.
+                    In this project, you can display and calculate the nutritional values of a specific food item.
                     You can also filter items by the given tags and combine these tags to get a more specific search result.
                 </p>
                 <p>
-                    All data was colelcted by myslef and I stored the data in a mySQL database.
-                    For this project however, I will fetch the data from a .json file, since I didn't deploy my backend project yet.
+                    All data was collected by myself and I stored it in a mySQL database.
+                    However, for this project I will fetch the data from a .json file, since I haven't deployed my backend project yet.
+                    For now, I only have 35 food-items in my database, but I think this is more than enough to showcase the basic usage and functionalities of my application.
                 </p>
 
-                <p>In the future you should also be able to save one or more food items to your own personal food diary.</p>
+                <p>
+                    In the future, I want to implement a diary, where you should be able to save one or more food items to your own personal food diary.
+                </p>
 
                 <p>My ideas for the diary section:</p>
 
-                <li>Keep track of all food items that the user has had added before</li>
+                <li>Keep track of all food items that the user has added before</li>
                 <li>Create a calendar to sort all diary entries by date</li>
                 <li>Edit and delete a diary entry</li>
             </section>
@@ -43,25 +46,26 @@ export const FoodApp: React.FC = () => {
                 <ul className="NavBar">
                     <li>
                         <div className="hoverEffect">
-                            <Link to="/">Home</Link>
+                            <Link to="/food-demo/home">Home</Link>
                         </div>
                     </li>
                     <li>
                         <div className="hoverEffect">
-                            <Link to="/food">Food</Link>
+                            <Link to="/food-demo/food">Food</Link>
                         </div>
                     </li>
                     <li>
                         <div className="hoverEffect">
-                            <Link to="/diary">Diary</Link>
+                            <Link to="/food-demo/diary">Diary</Link>
                         </div>
                     </li>
                 </ul>
 
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/food" element={<FoodPage />} />
-                    <Route path="/diary" element={<DiaryPage />} />
+                    <Route path="*" element={<Navigate to="/food-demo/home" />} />
+                    <Route path="/food-demo/home" element={<Home />} />
+                    <Route path="/food-demo/food" element={<FoodPage />} />
+                    <Route path="/food-demo/diary" element={<DiaryPage />} />
                 </Routes>
             </div>
         </BrowserRouter>
